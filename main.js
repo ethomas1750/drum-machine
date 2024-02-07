232a69495f8c1357a389b075edc1eb2096753853
ethomas1750 <144511038+ethomas1750@users.noreply.github.com>
2024-02-07 11:10:13 -0600

    Update main.js

--- main.js
+++ main.js
@@ -18,21 +18,6 @@ tick.play()
 count ++
 let currentCount = document.querySelector('#mCount')
 currentCount.innerText = `${count % 4}`
-def arrow_left(event):
-    global tempo, scale, tempo_range
-    if tempo - 10 >= tempo_range[0]:
-        tempo -= 10
-    else:
-        tempo -= (tempo-tempo_range[0])
-    scale.set(tempo) 
-
-def arrow_right(event):
-    global tempo, scale, tempo_range
-    if tempo + 10 <= tempo_range[1]:
-        tempo += 10
-    else:
-        tempo += (tempo_range[1]-tempo)
-    scale.set(tempo) 
 
 // This function sets up update() to be called every 600ms
 function setupUpdate() {
